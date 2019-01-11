43d12e8d103c57dc8d4a3c707d761c50b938c325
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2019-01-11 10:59:36 -0700

    Refactoring truffle test to work with latest version of web3

--- ipfs/ipfs-ethereum-tutorial/test/IPFSInboxTests.js
+++ ipfs/ipfs-ethereum-tutorial/test/IPFSInboxTests.js
@@ -1,24 +1,18 @@
 const IPFSInbox = artifacts.require("./IPFSInbox.sol")
+const truffleAssert = require('../client/node_modules/truffle-assertions')
 
 contract("IPFSInbox", accounts => {
   it("...should emit an event when you send an IPFS address.",
   async () => {
-    
     // Wait for the contract to be deployed
     const ipfsInbox = await IPFSInbox.deployed()
 
-    // Set a variable to false, and create an event listener
-    // to set it to true if the event fires.
-    eventEmitted = false
-    var event = ipfsInbox.ipfsSent()
-    await event.watch((err, res) => {
-      eventEmitted = true
-    })
-
     // Call the contract function which sends an IPFS address
-    await ipfsInbox.sendIPFS(accounts[1], "SampleAddress", { from: accounts[0] })
+    const sent = await ipfsInbox.sendIPFS(accounts[1], "SampleAddress", { from: accounts[0] })
 
-    // Check if the variable is set to true by this time
-    assert.equal(eventEmitted, true, "Sending an IPFS request emits an event")
+    // Assert the correct event was emitted
+    truffleAssert.eventEmitted(sent, 'ipfsSent', (ev) => {
+      return ev._ipfsHash == "SampleAddress"
+    })
   })
 })
